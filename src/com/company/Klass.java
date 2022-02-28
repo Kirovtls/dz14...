fd44c1a9426df9b98910c2ff1707330ec1f76a99
rakhat nazarkulova <rakhatnazarkulova@rakhats-MacBook-Air.local>
2022-02-28 05:27:34 -0600

    Initial commit

---
+++ src/com/company/Klass.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Klass {
+
+        private int nomer;
+        private String slovo;
+        private int[] nomer2;
+
+        public Klass(int nomer, String slovo, int[] nomer2) {
+            this.nomer = nomer;
+            this.slovo = slovo;
+            this.nomer2 = nomer2;
+        }
+
+        public int getNomer() {
+            return nomer;
+        }
+
+        public void setNomer(int nomer) {
+            this.nomer = nomer;
+        }
+
+        public String getSlovo() {
+            return slovo;
+        }
+
+        public void setSlovo(String slovo) {
+            this.slovo = slovo;
+        }
+
+        public int[] getNomer2() {
+            return nomer2;
+        }
+
+        public void setNomer2(int[] nomer2) {
+            this.nomer2 = nomer2;
+        }
+    }
+
